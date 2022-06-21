5b6de3d217134e164110e21b61370b780ade9c99
Tristan Collis <tristan.collis@gmail.com>
2022-06-21 01:33:41 -0700

    Added command functionality, and figured out how to restrict commands to specific roles.

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+import discord
+import discord.utils
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix="`")
+
+
+@bot.event
+async def on_ready():
+    print(f"Connected as {bot.user}")
+
+
+@bot.command()
+async def test(ctx: commands.Context):
+    await ctx.send("Tested")
+
+
+@bot.command()
+@commands.has_role("Keeper")
+async def keeper(ctx: commands.Context):
+    await ctx.send("You're a Keeper!")
+
+
+def main():
+    bot.run("OTg4NjkxODUyNDczNTUyOTI3.G67Bnz.0n3YOtAh0NSOY7KANZmfj65WkLXfN-51xEPmmo")
+
+
+if __name__ == "__main__":
+    main()
